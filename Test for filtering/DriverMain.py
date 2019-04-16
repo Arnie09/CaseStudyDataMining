88873e4605d6717a5c1f4b6daadf601a72322fa8
Arnab Chanda <arnabchanda964@gmail.com>
2019-04-16 15:32:28 +0530

    New dataframe with manipulated data has been added

--- Test for filtering/DriverMain.py
+++ Test for filtering/DriverMain.py
@@ -18,6 +18,7 @@ for index in dataset.index:
     MAIN_LIST[index].append(dataset.loc[index]['Data Structure & Algorithm'])
     MAIN_LIST[index].append(dataset.loc[index]['Design & Analysis of Algorithm'])
     MAIN_LIST[index].append(dataset.loc[index]['Object Oriented Programming'])
+    #MAIN_LIST[index].append(dataset.loc[index]['Introduction to Computing'])
 
 #print(MAIN_LIST)
 
@@ -26,7 +27,7 @@ FptreeObject.displayRules(confmin=0.0,confmax = 1.0)
 rules = FptreeObject.rulesMainDict
 for rule in  rules:
     #print((rule))
-    if('Data Structure & Algorithm' in rule and 'Design & Analysis of Algorithm' in rule and 'Object Oriented Programming' in rules[rule]):
+    if('Data Structure & Algorithm' in rule or 'Design & Analysis of Algorithm' in rule and 'Object Oriented Programming' in rules[rule]):
         print(rule+" : "+rules[rule])
     # for elements in rule:
     #     print(elements)
